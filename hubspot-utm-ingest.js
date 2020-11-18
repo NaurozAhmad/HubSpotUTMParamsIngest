fc22f361c4f0ec3eda8a297f93d6e545681d8934
NaurozAhmad <ahmadnauroz@gmail.com>
2020-11-18 23:58:19 +0500

    refactored code so ip address is always populated regardless of expiry

--- hubspot-utm-ingest.js
+++ hubspot-utm-ingest.js
@@ -4,7 +4,7 @@
   if (urlParams.get('utm_campaign') || urlParams.get('utm_medium') || urlParams.get('utm_source') || urlParams.get('gclid')) {
     console.log('has query params');
     let expires = new Date();
-    expires.setMinutes(expires.getMinutes() + 10800);
+    expires.setMinutes(expires.getMinutes() + 43200);
     console.log('expires', expires);
     if (urlParams.get('gclid')) {
       localStorage.removeItem('utm_source');
@@ -27,62 +27,60 @@
       setTimeout(function() {
         let expiry = new Date(localStorage.getItem('expiry'));
         let now = new Date();
-        console.log("Form loaded!");
-        if (expiry.getTime() < now.getTime()) {
-          console.log('expired');
-        } else {
-          setTimeout(function() {
-            console.log('setting values of hubspot form.');
-            let myiframe = document.getElementById('hs-form-iframe-0');
-            console.log('myiframe', myiframe);
-            var y = (myiframe.contentWindow || myiframe.contentDocument);
-            console.log('y', y.document);
-            let inputs = y.document.getElementsByClassName('hs-input')
-            console.log('utm', inputs);
-            let utm_campaign = '';
-            let utm_guid = '';
-            let ip = '';
-            if (localStorage.getItem('utm_campaign')) {
-              utm_campaign = localStorage.getItem('utm_campaign').split('-')[0];
-              utm_guid = localStorage.getItem('utm_campaign').split('-')[1] || '';
-            }
+        let expired = expiry.getTime() < now.getTime() ? true : false;
+        console.log('expired?', expired);
+        console.log('Form loaded!');
+        setTimeout(function() {
+          console.log('setting values of hubspot form.');
+          let myiframe = document.getElementById('hs-form-iframe-0');
+          console.log('myiframe', myiframe);
+          var y = (myiframe.contentWindow || myiframe.contentDocument);
+          console.log('y', y.document);
+          let inputs = y.document.getElementsByClassName('hs-input')
+          console.log('utm', inputs);
+          let utm_campaign = '';
+          let utm_guid = '';
+          let ip = '';
+          if (localStorage.getItem('utm_campaign')) {
+            utm_campaign = localStorage.getItem('utm_campaign').split('-')[0];
+            utm_guid = localStorage.getItem('utm_campaign').split('-')[1] || '';
+          }
 
-            function onDataRecieve() {
-              const ipRegex = /[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/
-              ip = xhttp.responseText.match(ipRegex)[0];
-            }
-            const xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = onDataRecieve;
-            xhttp.open('GET', `https://www.cloudflare.com/cdn-cgi/trace`, true);
-            xhttp.send();
+          function onDataRecieve() {
+            const ipRegex = /[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/
+            ip = xhttp.responseText.match(ipRegex)[0];
+          }
+          const xhttp = new XMLHttpRequest();
+          xhttp.onreadystatechange = onDataRecieve;
+          xhttp.open('GET', `https://www.cloudflare.com/cdn-cgi/trace`, true);
+          xhttp.send();
 
-            for (var i = 0; i < inputs.length; i++) {
-              if (localStorage.getItem('gclid')) {
-                console.log('adding gclid', localStorage.getItem('gclid'));
-                if (inputs[i].name == 'gclid') {
-                  inputs[i].value = localStorage.getItem('gclid');
-                }
-              } else {
-                console.log('adding utm params', localStorage.getItem('utm_campaign'));
-                if (inputs[i].name == 'utm_campaign') {
-                  inputs[i].value = utm_campaign;
-                }
-                if (inputs[i].name == 'utm_source') {
-                  inputs[i].value = localStorage.getItem('utm_source');
-                }
-                if (inputs[i].name == 'utm_medium') {
-                  inputs[i].value = localStorage.getItem('utm_medium');
-                }
-                if (inputs[i].name == 'utm_guid') {
-                  inputs[i].value = utm_guid;
-                }
-                if (inputs[i].name == 'ip_address') {
-                  inputs[i].value = ip;
-                }
+          for (var i = 0; i < inputs.length; i++) {
+            if (localStorage.getItem('gclid')) {
+              console.log('adding gclid', localStorage.getItem('gclid'));
+              if (inputs[i].name == 'gclid') {
+                inputs[i].value = !expired ? localStorage.getItem('gclid') : '';
+              }
+            } else {
+              console.log('adding utm params', localStorage.getItem('utm_campaign'));
+              if (inputs[i].name == 'utm_campaign') {
+                inputs[i].value = !expired ? utm_campaign : '';
+              }
+              if (inputs[i].name == 'utm_source') {
+                inputs[i].value = !expired ? localStorage.getItem('utm_source') : '';
+              }
+              if (inputs[i].name == 'utm_medium') {
+                inputs[i].value = !expired ? localStorage.getItem('utm_medium') : '';
+              }
+              if (inputs[i].name == 'utm_guid') {
+                inputs[i].value = !expired ? utm_guid : '';
+              }
+              if (inputs[i].name == 'ip_address') {
+                inputs[i].value = ip;
               }
             }
-          }, 1000);
-        }
+          }
+        }, 1000);
       }, 1000);
     }
   });
