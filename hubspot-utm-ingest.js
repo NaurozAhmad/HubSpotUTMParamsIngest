8b0a3551da15278722880eb23199b7016afc195f
NaurozAhmad <ahmadnauroz@gmail.com>
2020-11-18 23:48:22 +0500

    added ip address input

--- hubspot-utm-ingest.js
+++ hubspot-utm-ingest.js
@@ -41,10 +41,21 @@
             console.log('utm', inputs);
             let utm_campaign = '';
             let utm_guid = '';
+            let ip = '';
             if (localStorage.getItem('utm_campaign')) {
               utm_campaign = localStorage.getItem('utm_campaign').split('-')[0];
               utm_guid = localStorage.getItem('utm_campaign').split('-')[1] || '';
             }
+
+            function onDataRecieve() {
+              const ipRegex = /[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/
+              ip = xhttp.responseText.match(ipRegex)[0];
+            }
+            const xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = onDataRecieve;
+            xhttp.open('GET', `https://www.cloudflare.com/cdn-cgi/trace`, true);
+            xhttp.send();
+
             for (var i = 0; i < inputs.length; i++) {
               if (localStorage.getItem('gclid')) {
                 console.log('adding gclid', localStorage.getItem('gclid'));
@@ -64,6 +75,9 @@
                 }
                 if (inputs[i].name == 'utm_guid') {
                   inputs[i].value = utm_guid;
+                }
+                if (inputs[i].name == 'ip_address') {
+                  inputs[i].value = ip;
                 }
               }
             }
